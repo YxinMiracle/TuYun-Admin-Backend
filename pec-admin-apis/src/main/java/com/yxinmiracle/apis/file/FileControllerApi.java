92a6c7605076b41660f6b38a4dcecf6365bd0bc8
YxinMiracle <939157765@qq.com>
2022-03-25 17:09:41 +0800

    add file fuction

--- pec-admin-apis/src/main/java/com/yxinmiracle/apis/file/FileControllerApi.java
+++ pec-admin-apis/src/main/java/com/yxinmiracle/apis/file/FileControllerApi.java
@@ -1,7 +1,6 @@
 package com.yxinmiracle.apis.file;
 
 import com.yxinmiracle.model.common.dtos.ResponseResult;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileControllerApi {
